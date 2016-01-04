e4cce5caa22cacee1e0d76f575818aae1f37e7e5
John Leach <john@johnleach.co.uk>
2016-01-04 14:17:24 +0000

    Update references to 2.3

--- debian/split-tk-out.rb
+++ debian/split-tk-out.rb
@@ -4,8 +4,8 @@ require 'fileutils'
 
 include FileUtils::Verbose
 
-src_package = 'libruby2.2'
-dst_package = 'ruby2.2-tcltk'
+src_package = 'libruby2.3'
+dst_package = 'ruby2.3-tcltk'
 
 # .so files
 tk_so_files = Dir.glob("debian/#{src_package}/usr/lib/*/ruby/**/{tcltklib,tkutil}.so")
@@ -18,10 +18,10 @@ end
 # .rb files
 tk_lib_files = Dir.chdir('ext/tk/lib') { Dir.glob('*').select { |f| File.directory?(f) || f =~ /\.rb$/ } }
 
-mkdir_p "debian/#{dst_package}/usr/lib/ruby/2.2.0"
+mkdir_p "debian/#{dst_package}/usr/lib/ruby/2.3.0"
 tk_lib_files.each do |f|
-  src = File.join("debian/#{src_package}/usr/lib/ruby/2.2.0/#{f}")
-  dst = File.join("debian/#{dst_package}/usr/lib/ruby/2.2.0/#{f}")
+  src = File.join("debian/#{src_package}/usr/lib/ruby/2.3.0/#{f}")
+  dst = File.join("debian/#{dst_package}/usr/lib/ruby/2.3.0/#{f}")
   mv src, dst
 end
 
